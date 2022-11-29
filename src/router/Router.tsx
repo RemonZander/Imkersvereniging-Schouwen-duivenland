dff9b7037eb30a364d7fe515513504f44a5b700b
Rayan1159 <baskruithof999@gmail.com>
2022-11-29 19:20:58 +0100

    Router setup is done

---
+++ src/router/Router.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {
+    createBrowserRouter
+} from "react-router-dom";
+
+const paths = [
+    {
+        path: ""
+    }
+]
+export const router = createBrowserRouter(paths)
