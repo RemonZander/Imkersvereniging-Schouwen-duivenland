284faa2d3ac21565ae468a7a67caa6cf53b27eca
unknown <baskruithof999@gmail.com>
2023-01-10 12:51:57 +0100

    changed from class based to function based

--- src/layout/additional/header/header.tsx
+++ src/layout/additional/header/header.tsx
@@ -5,42 +5,38 @@ import {
     Link
 } from "react-router-dom";
 
-class Header extends Component {
-
-    render(): JSX.Element {
-        return (
-            <div className="flex w-screen bg-black text-white align-center">
-                <div className="m-auto pb-[1%] pt-[1%]">
-                    <ul className="flex flex-row gap-3 text-yellow-700">
-                        <li className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
-                            <Link to="/projecten">Projecten</Link>
-                        </li>
-                        <li id="agenda" className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
-                            <Link to="/agenda">Agenda</Link>
-                        </li>
-                        <li  className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
-                            <Link to="/nieuws">Nieuws</Link>
-                        </li>
-                        <li className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
-                            <Link to="/vrienden">Vrienden</Link>
-                        </li>
-                        <li className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
-                            <Link to="/bijenzwerm">Bijenzwerm</Link>
-                        </li>
-                        <li className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
-                            <Link to="/bijenhouden">Bijenhouden</Link>
-                        </li>
-                        <li className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
-                            <Link to="/informatie">Informatie</Link>
-                        </li>
-                        <li className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
-                            <Link to="/contact">Contact</Link>
-                        </li>
-                    </ul>
-                </div>
+const Header = (): JSX.Element => {
+    return (
+        <div className="flex w-screen bg-black text-white align-center">
+            <div className="m-auto pb-[1%] pt-[1%]">
+                <ul className="flex flex-row gap-3 text-yellow-700">
+                    <li className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
+                        <Link to="/projecten">Projecten</Link>
+                    </li>
+                    <li id="agenda" className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
+                        <Link to="/agenda">Agenda</Link>
+                    </li>
+                    <li  className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
+                        <Link to="/nieuws">Nieuws</Link>
+                    </li>
+                    <li className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
+                        <Link to="/vrienden">Vrienden</Link>
+                    </li>
+                    <li className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
+                        <Link to="/bijenzwerm">Bijenzwerm</Link>
+                    </li>
+                    <li className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
+                        <Link to="/bijenhouden">Bijenhouden</Link>
+                    </li>
+                    <li className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
+                        <Link to="/informatie">Informatie</Link>
+                    </li>
+                    <li className="border border-yellow-700 pl-[1.5%] pr-[1.5%] hover:text-brown">
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
             </div>
-        )
-    }
+        </div>
+    )
 }
-
 export default Header;
